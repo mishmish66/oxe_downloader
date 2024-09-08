6e84a6fa9b6798bc16e8535822717502fe0fe451
Misha <m.lvovsky66@gmail.com>
2024-09-08 01:51:16 -0400

    bumped version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="oxe_downloader",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     include_package_data=True,
     package_data={"": ["*.json"]},
