99c80c8d8def111e46a8d10e519536053a2d20b7
Misha <m.lvovsky66@gmail.com>
2024-09-08 01:48:16 -0400

    added json to package data

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
+    package_data={"": ["*.json"]},
     install_requires=[
         "typer",
         "google-cloud-storage",
